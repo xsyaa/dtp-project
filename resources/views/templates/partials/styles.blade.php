9dab2a828043070b4ff2134c633657d15044ba41
amelia <nabilasyafaamelia@gmail.com>
2022-09-29 14:39:35 +0700

    first commit

---
+++ resources/views/templates/partials/styles.blade.php
@@ -0,0 +1,6 @@
+<!-- CSS files -->
+    <link href="./dist/css/tabler.min.css" rel="stylesheet"/>
+    <link href="./dist/css/tabler-flags.min.css" rel="stylesheet"/>
+    <link href="./dist/css/tabler-payments.min.css" rel="stylesheet"/>
+    <link href="./dist/css/tabler-vendors.min.css" rel="stylesheet"/>
+    <link href="./dist/css/demo.min.css" rel="stylesheet"/>
